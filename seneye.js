e1a4bed25c222e010b0899b2c5c6fd933d1ec4c5
Mathieu Dardenne <mathieu@dardenne.fr>
2017-09-05 20:01:01 +0800

    Adding events handling

--- seneye.js
+++ seneye.js
@@ -3,6 +3,7 @@
  */
 var usb      = require('node-hid');
 var Bitfield = require('bitfield');
+var events 	 = require('events');
 
 var SENEYE_VENDOR_ID  = 9463;
 var SENEYE_PRODUCT_ID = 8708;
@@ -13,13 +14,13 @@ var readBits = function(bitfield, offset, size) {
 	for (var i = 0; i < size; i++)
 		bits += bitfield.get(offset + i) ? '1' : '0';
 	return bits;
-}
+};
 
 var SeneyeJS = function() {
 
-	var in0, out0, device;
-	var onStart, onRead;
-	var _self = this;
+	var device;
+	var _self 		 = this;
+	var eventEmitter = new events.EventEmitter();
 
 	var onData = function(buf) {
 
@@ -27,9 +28,9 @@ var SeneyeJS = function() {
 		var code = buf.readUIntLE(1, 1);
 
 		if (action == 136 && code == 1) {
-			onStart(_self, parseHello(buf));
+			eventEmitter.emit('hello', _self, parseHello(buf));
 		} else if (action == 0 && code == 1) {
-			onRead(_self, parseReading(buf));
+			eventEmitter.emit('complete',  parseReading(buf));
 		} else if (action == 0 && code == 2) {
 		} else if (action == 119 && code == 1) {
 			parseGoodbye(buf);
@@ -95,10 +96,7 @@ var SeneyeJS = function() {
 		var type = ["Home", "Home", "Pound", "Reef"];
 		var version = (d[5] << 8) + d[4];
 
-		return {
-			type: type[d[3]],
-			version: version / 10000
-		};
+		return { type: type[d[3]], version: version / 10000 };
 	};
 
 	var parseGoodbye = function(buff) {
@@ -109,45 +107,42 @@ var SeneyeJS = function() {
 		onClose(_self, d);
 	};
 
+	this.read = function() {
+		writeData(TO_HEX_INT_ARR("HELLOSUD"));
+		return _self;
+	};
+
 	this.goodbye = function() {
 		writeData(TO_HEX_INT_ARR("BYESUD"));
+		return _self;
 	};
 
 	this.reading = function() {
 		writeData(TO_HEX_INT_ARR("READING"));
+		return _self;
 	};
 
-	this.start = function(cbStart, cbRead, cbClose) {
-
-		device  = new usb.HID(SENEYE_VENDOR_ID, SENEYE_PRODUCT_ID);
-
-		onStart = cbStart;
-		onRead  = cbRead;
-		onClose = cbClose;
-
-		if (!device) {
-			console.log("Device not found.");
-			return 1;
-		}
-
-		device.on('data',  onData);
-		device.on('error', onError);
-		device.on('end',   onEnd);
-
-		writeData(TO_HEX_INT_ARR("HELLOSUD"));
+	this.on = function(action, callback) {
+		eventEmitter.on(action, callback);
+		return _self;
 	};
+
+	device  = new usb.HID(SENEYE_VENDOR_ID, SENEYE_PRODUCT_ID);
+
+	if (!device) {
+		console.log("Device not found.");
+		return 1;
+	}
+
+	device.on('data',  onData);
+	device.on('error', onError);
+	device.on('end',   onEnd);
+
+	this.on('hello', this.reading);
+	this.on('complete', this.goodbye);
 
 	return this;
-}();
 
-// usb.setDebugLevel(4);
+};
 
-SeneyeJS.start((s, d) => {
-	console.log(d);
-	s.reading();
-}, (s, d) => {
-	console.log(d);
-	s.goodbye();
-}, (s, d) => {
-	console.log('Goodbye');
-});
+module.exports.default = SeneyeJS;
